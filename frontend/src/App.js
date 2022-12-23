ed4fe1f38cec5f5ec1c2d4f45ad6f2237dcd9687
mmaddex <mmaddex@render.com>
2022-12-23 10:53:51 -0800

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>        
+        <p>{!data ? "Seeing..." : data}</p>
       </header>
     </div>
   );
